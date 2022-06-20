e4543e71722cc38f059aec47d1a1eda17a8d4cd4
makoefer <makoefer@edu.aau.at>
2022-06-20 09:34:43 +0200

    Initial commit

---
+++ app/src/main/java/martin/scorecounter/MainActivity.kt
@@ -0,0 +1,11 @@
+package martin.scorecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
